812f30d125152e354d23800d46286bcd2546051c
Francis <info@francisqureshi.com>
2026-01-19 23:31:55 +0000

    media

--- src/io/sources.zig
+++ src/io/sources.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 const media = @import("media.zig");
 
-// struct or array ?
-const sources = struct {};
+const allocator = std.mem.Allocator;
+
+const source_list = std.ArrayList(media.SourceMedia);
+
+pub fn importSource(sl: source_list, sm: media.SourceMedia) void {
+    sl.append(
+        allocator,
+        sm,
+    );
+}
