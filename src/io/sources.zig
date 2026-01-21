b4afb43d91868d7bb1dd97ad2427c10e6419d5f7
francisqureshi <info@francisqureshi.com>
2026-01-21 10:29:11 +0000

    sources wip

--- src/io/sources.zig
+++ src/io/sources.zig
@@ -3,11 +3,6 @@ const media = @import("media.zig");
 
 const allocator = std.mem.Allocator;
 
-const source_list = std.ArrayList(media.SourceMedia);
-
-pub fn importSource(sl: source_list, sm: media.SourceMedia) void {
-    sl.append(
-        allocator,
-        sm,
-    );
-}
+const source_pool = struct {
+    sources: std.ArrayHashMapUnmanaged(dbhash, *media.SourceMedia),
+};
